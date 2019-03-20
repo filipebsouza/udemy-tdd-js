5e26bc8742521fc2c3438329ad0c4f05b6ae858e
Filipe <filipebsouza@live.com>
2019-03-20 02:16:07 +0000

    novos testes

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -23,11 +23,29 @@ describe('Testes do objeto Spy', function() {
         }
     }
 
+    const calculadora4 = {
+        somar: function(n1, n2) {
+            return n1 + n2;
+        },
+        subtrair: function(n1, n2) {
+            return n1 - n2;
+        }
+    }
+
+    const calculadora5 = {
+        somar: function(n1, n2) {
+            return n1 + n2;
+        }
+    }
+
     beforeAll(function() {
         spyOn(calculadora, 'somar');
         spyOn(calculadora1, 'somar');
         spyOn(calculadora2, 'somar');
         spyOn(calculadora3, 'somar');
+        spyOn(calculadora4, 'somar').and.callThrough();
+        spyOn(calculadora4, 'subtrair');
+        spyOn(calculadora5, 'somar').and.returnValue(10);
     });
 
     it('deve possuir o método somar como não definido', function() {
@@ -54,5 +72,16 @@ describe('Testes do objeto Spy', function() {
         calculadora3.somar(1, 2);
         expect(calculadora3.somar).toHaveBeenCalledWith(1, 1);
         expect(calculadora3.somar).toHaveBeenCalledWith(1, 2);
+    });
+
+     //and.callThrough, vai chamar o método realmente, não vai simular ou usar o mock, vai usar o cara mesmo
+     it('deve chamar o método somar original', function() {
+        expect(calculadora4.somar(1, 1)).toEqual(2);
+        expect(calculadora4.subtrair(2, 1)).toBeUndefined();
+    });
+
+     //and.returnValue, simula um retorno do método
+     it('deve retornar 10 para o método somar', function() {
+        expect(calculadora5.somar(1, 9)).toEqual(10);
     });
 });
