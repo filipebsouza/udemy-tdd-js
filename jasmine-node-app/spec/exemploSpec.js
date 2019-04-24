30768cef9574605022e245e0efc170509e6883ab
Filipe Souza <filipe.souza@db1.com.br>
2019-04-24 13:24:43 -0300

    Karma configurado

--- jasmine-node-app/spec/exemploSpec.js
+++ jasmine-node-app/spec/exemploSpec.js
@@ -2,4 +2,8 @@ describe('exemplo de teste', () => {
     it('teste de exemplo', () => {
         expect(true).toEqual(true);
     });
+
+    it('deve demonstrar o uso do karma em modo de escuta', () => {
+        expect(true).toEqual(true);
+    });
 });
