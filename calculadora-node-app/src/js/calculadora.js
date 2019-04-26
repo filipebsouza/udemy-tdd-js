7b1cdc4b281bfeac2115917bba2977e9d1e4c9a4
Filipe Souza <filipe.souza@db1.com.br>
2019-04-26 16:56:59 -0300

    Refatoracao

--- calculadora-node-app/src/js/calculadora.js
+++ calculadora-node-app/src/js/calculadora.js
@@ -1,40 +1,56 @@
 const Calculadora = {
-    adicionar: (numero1, numero2) => {
-        if (isNaN(numero1) || isNaN(numero2)) {
-            return 0;
-        }
+    ADICAO: '+',
+    SUBTRACAO: '-',
+    DIVISAO: '/',
+    MULTIPLICACAO: '*',
+
+    calcular: (numero1, numero2, operacao) => {
+        let resultado = 0;
 
         numero1 = parseFloat(numero1);
         numero2 = parseFloat(numero2);
 
-        return numero1 + numero2;
+        if (isNaN(numero1) || isNaN(numero2)) {
+            return 0;
+        }
+
+        switch (operacao) {
+            case Calculadora.ADICAO:
+                resultado = numero1 + numero2;
+                break;
+            case Calculadora.SUBTRACAO:
+                resultado = numero1 - numero2;
+                break;
+            case Calculadora.DIVISAO:
+                resultado = numero1 / numero2;
+                break;
+            case Calculadora.MULTIPLICACAO:
+                resultado = numero1 * numero2;
+                break;
+            default:
+                resultado = 0;
+        }
+
+        return resultado;
+    },
+
+    adicionar: (numero1, numero2) => {
+        return Calculadora.calcular(numero1, numero2, Calculadora.ADICAO);
     },
 
     subtrair: (numero1, numero2) => {
-        if (isNaN(numero1) || isNaN(numero2)) {
-            return 0;
-        }
-
-        return numero1 - numero2;
+        return Calculadora.calcular(numero1, numero2, Calculadora.SUBTRACAO);
     },
 
     dividir: (numero1, numero2) => {
-        if (isNaN(numero1) || isNaN(numero2)) {
-            return 0;
-        }
-
         if (numero2 == 0)
             return "Erro";
 
-        return numero1 / numero2;
+        return Calculadora.calcular(numero1, numero2, Calculadora.DIVISAO);
     },
 
     multiplicar: (numero1, numero2) => {
-        if (isNaN(numero1) || isNaN(numero2)) {
-            return 0;
-        }
-        
-        return numero1 * numero2;
+        return Calculadora.calcular(numero1, numero2, Calculadora.MULTIPLICACAO);
     }
 }
 
